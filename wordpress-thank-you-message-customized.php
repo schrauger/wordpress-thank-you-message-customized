e51266853487b6df493f7a224c99f51e3d83dba3
Stephen Schrauger <6665521+schrauger@users.noreply.github.com>
2025-12-16 01:31:44 -0500

    Fix namespace resolution in donor message plugin
    Updated function calls to include backslashes for proper namespace resolution in the WordPress plugin.

--- wordpress-thank-you-message-customized.php
+++ wordpress-thank-you-message-customized.php
@@ -21,9 +21,9 @@ const SHORTCODE_SLUG  = 'donor_message';
 const BLOCK_NAMESPACE = 'donor-thank-you';
 
 // create the CPT
-add_action('init', __NAMESPACE__ . '\\register_cpt', 0);
+\add_action('init', __NAMESPACE__ . '\\register_cpt', 0);
 function register_cpt() {
-    register_post_type(CPT_SLUG, [
+    \register_post_type(CPT_SLUG, [
         'labels' => [
             'name'          => __('Donor Messages', 'donor-thank-you'),
             'singular_name' => __('Donor Message', 'donor-thank-you'),
@@ -36,7 +36,7 @@ function register_cpt() {
 }
 
 // save Editor donor information
-add_action('save_post_' . CPT_SLUG, __NAMESPACE__ . '\\generate_donor_token');
+\add_action('save_post_' . CPT_SLUG, __NAMESPACE__ . '\\generate_donor_token');
 function generate_donor_token($post_id) {
     // avoid autosave / revisions
     if (\defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
@@ -47,7 +47,7 @@ function generate_donor_token($post_id) {
 }
 
 // Editor side meta box, to show the generated url to the Editor to copy and paste or use elsewhere
-add_action('add_meta_boxes', __NAMESPACE__ . '\\add_donor_meta_box');
+\add_action('add_meta_boxes', __NAMESPACE__ . '\\add_donor_meta_box');
 function add_donor_meta_box() {
     add_meta_box(
         'donor_link_meta',
@@ -68,9 +68,9 @@ function render_donor_meta_box($post) {
 }
 
 // admin preference to specify page path
-add_action('admin_menu', __NAMESPACE__ . '\\add_admin_menu');
+\add_action('admin_menu', __NAMESPACE__ . '\\add_admin_menu');
 function add_admin_menu() {
-    add_submenu_page(
+    \add_submenu_page(
         'edit.php?post_type=' . CPT_SLUG, // parent slug -> make it appear under CPT menu
         'Donor Settings',         // page title
         'Settings',               // menu title
@@ -82,11 +82,11 @@ function add_admin_menu() {
 
 function donor_settings_page() {
     if (isset($_POST['donor_settings_nonce']) && \wp_verify_nonce($_POST['donor_settings_nonce'], 'save_donor_settings')) {
-        update_option('donor_thank_you_page_path', sanitize_text_field($_POST['donor_page_path']));
-        update_option('donor_generic_title', sanitize_text_field($_POST['donor_generic_title']));
-        update_option('donor_generic_message', sanitize_textarea_field($_POST['donor_generic_message']));
-        update_option('donor_pre_name_text', sanitize_text_field($_POST['donor_pre_name_text']));
-        update_option('donor_post_name_text', sanitize_text_field($_POST['donor_post_name_text']));
+        \update_option('donor_thank_you_page_path', \sanitize_text_field($_POST['donor_page_path']));
+        \update_option('donor_generic_title', \sanitize_text_field($_POST['donor_generic_title']));
+        \update_option('donor_generic_message', \sanitize_textarea_field($_POST['donor_generic_message']));
+        \update_option('donor_pre_name_text', \sanitize_text_field($_POST['donor_pre_name_text']));
+        \update_option('donor_post_name_text', \sanitize_text_field($_POST['donor_post_name_text']));
         echo '<div class="updated"><p>Settings saved.</p></div>';
     }
 
@@ -118,26 +118,26 @@ function donor_settings_page() {
                 <tr>
                     <th scope="row"><label for="donor_generic_message">Generic Thank You Message</label></th>
                     <td>
-                        <textarea name="donor_generic_message" id="donor_generic_message" rows="5" class="large-text"><?php echo esc_textarea($generic_message); ?></textarea>
+                        <textarea name="donor_generic_message" id="donor_generic_message" rows="5" class="large-text"><?php echo \esc_textarea($generic_message); ?></textarea>
                         <p class="description">Message shown when donor ID is missing or invalid. HTML is allowed.</p>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="donor_pre_name_text">Pre-Name Text</label></th>
                     <td>
-                        <input type="text" name="donor_pre_name_text" id="donor_pre_name_text" value="<?php echo esc_attr($pre_name_text); ?>" class="regular-text">
+                        <input type="text" name="donor_pre_name_text" id="donor_pre_name_text" value="<?php echo \esc_attr($pre_name_text); ?>" class="regular-text">
                         <p class="description">Text displayed before the donor name in personalized messages.</p>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="donor_post_name_text">Post-Name Text</label></th>
                     <td>
-                        <input type="text" name="donor_post_name_text" id="donor_post_name_text" value="<?php echo esc_attr($post_name_text); ?>" class="regular-text">
+                        <input type="text" name="donor_post_name_text" id="donor_post_name_text" value="<?php echo \esc_attr($post_name_text); ?>" class="regular-text">
                         <p class="description">Text displayed after the donor name in personalized messages.</p>
                     </td>
                 </tr>
             </table>
-            <?php submit_button(); ?>
+            <?php \submit_button(); ?>
         </form>
     </div>
     <?php
@@ -154,10 +154,10 @@ function render_donor_shortcode() {
     $post_name_text  = \get_option('donor_post_name_text', 'for your support!');
 
     if (!isset($_GET['donorid'])) {
-        return '<h2>' . esc_html($generic_title) . '</h2><p>' . $generic_message . '</p>';
+        return '<h2>' . \esc_html($generic_title) . '</h2><p>' . $generic_message . '</p>';
     }
 
-    $token = sanitize_text_field($_GET['donorid']);
+    $token = \sanitize_text_field($_GET['donorid']);
 
     $query = new \WP_Query([
         'post_type'  => CPT_SLUG,
@@ -171,7 +171,7 @@ function render_donor_shortcode() {
     ]);
 
     if (!$query->have_posts()) {
-        return '<h2>' . esc_html($generic_title) . '</h2><p>' . $generic_message . '</p>';
+        return '<h2>' . \esc_html($generic_title) . '</h2><p>' . $generic_message . '</p>';
     }
 
     $query->the_post();
@@ -179,10 +179,10 @@ function render_donor_shortcode() {
     $message = \get_the_content();
     \wp_reset_postdata();
 
-    ob_start();
+    \ob_start();
     ?>
     <div class="donor-thank-you">
-        <h2><?php echo esc_html($pre_name_text . ' ' . $name . ' ' . $post_name_text); ?></h2>
+        <h2><?php echo \esc_html($pre_name_text . ' ' . $name . ' ' . $post_name_text); ?></h2>
         <div class="donor-message">
             <?php echo \apply_filters('the_content', $message); ?>
         </div>
@@ -194,11 +194,11 @@ function render_donor_shortcode() {
 
 
 // add a block so it's easier to add to a page than a shortcode
-add_action('init', __NAMESPACE__ . '\\register_donor_block');
+\add_action('init', __NAMESPACE__ . '\\register_donor_block');
 function register_donor_block() {
     if (!function_exists('register_block_type')) return;
 
-    register_block_type(BLOCK_NAMESPACE . '/message', [
+    \register_block_type(BLOCK_NAMESPACE . '/message', [
         'title'           => __('Donor Thank You', 'donor-thank-you'),
         'category'        => 'widgets',
         'icon'            => 'heart',
@@ -208,15 +208,15 @@ function register_donor_block() {
     ]);
 }
 function render_donor_block($attributes) {
-    return do_shortcode('[' . SHORTCODE_SLUG . ']');
+    return \do_shortcode('[' . SHORTCODE_SLUG . ']');
 }
 
 function enqueue_donor_block_assets() {
-    wp_register_script(
+    \wp_register_script(
         'donor-thank-you-block-js',
-        plugins_url('block.js', __FILE__),
+        \plugins_url('block.js', __FILE__),
         ['wp-blocks', 'wp-element', 'wp-editor'],
-        filemtime(plugin_dir_path(__FILE__) . 'block.js')
+        filemtime(\plugin_dir_path(__FILE__) . 'block.js')
     );
 }
-add_action('enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_donor_block_assets');
+\add_action('enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_donor_block_assets');
